25f802eaf384902411e5e02287275f661d2bf21f
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-30 14:51:56 -0400

    Set up project for heroku.

--- index.js
+++ index.js
@@ -47,3 +47,8 @@ app.route("/remove/:id").get((req, res) => {
         res.redirect("/");
     });
 });
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
