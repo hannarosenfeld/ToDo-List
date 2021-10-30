d64adea5322e7f01a37187bd2721d2ee0883209b
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-30 17:12:23 -0400

    Fix styling.

--- index.js
+++ index.js
@@ -48,7 +48,3 @@ app.route("/remove/:id").get((req, res) => {
     });
 });
 
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
