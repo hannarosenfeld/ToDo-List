2ef092c372af545d0f2eed26c66633308d33c560
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-31 14:07:23 -0400

    Test.

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require("express")
 const app = express()
 const mongodb = require("mongodb")
 const mongoose = require("mongoose")
-const TodoTask = require("./models/TodoTask");
+const TodoTask = require("./models/TodoTask")
+const dotenv = require("dotenv")
 //const cors = require("cors")
-
+dotenv.config()
 //app.use(cors());
 //app.use(express.json());
 
 //const path = require('path');
 
-console.log(process.env.MONGO_URI);
+console.log( typeof(process.env.MONGO_URI));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
